77bbf908cafd9df6e8507f2c4f7f1d904ed3b789
Jose Henrique Malta <shollxxz12@gmail.com>
2025-06-26 17:49:01 -0300

    init

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+from dotenv import dotenv_values
 
 mongo_uri = "mongodb+srv://malta:291107ze@cluster0.3u6t1r3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 mongo_client = MongoClient(mongo_uri)
@@ -8,4 +9,9 @@ test_collection = database['test_collection']
 
 person ={ "name": "Lucca de Enzo", "age": 12}
 test_collection.insert_one(person)
+
+config = dotenv_values('.env')
+mongo_uri = config['URI_MONGO_ATLAS']
+
+mongo_client = MongoClient(mongo_uri)
 
